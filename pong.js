765a7761349ecc439fc28a135b166ec4ed3e7e59
Mauricio Mateus <mauriciomateus_13@hotmail.com>
2022-12-14 16:23:24 -0300

    Update pong.js

--- pong.js
+++ pong.js
@@ -32,9 +32,9 @@ let raquetada;
 let ponto;
 
 function preload(){
-  trilha = loadSound("trilha.mp3");
-  ponto = loadSound("ponto.mp3");
-  raquetada = loadSound("raquetada.mp3");
+  trilha = loadSound("/sons/trilha.mp3");
+  ponto = loadSound("/sons/ponto.mp3");
+  raquetada = loadSound("/sons/raquetada.mp3");
 }
 
 
